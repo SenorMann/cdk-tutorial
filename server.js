eab45bafc8db43c0a322d0ace90ee83ad77adc2f
Malik Mahmud <malikmahmud@safeguardworld.com>
2022-07-06 12:04:46 +0100

    turn on lh server

--- server.js
+++ server.js
@@ -1,39 +1,30 @@
 const AWS = require("aws-sdk");
 const { createApp } = require('@lhci/server');
 const serverless = require('serverless-http');
-const express = require("express");
 
-
+let handler;
 const secrets = new AWS.SecretsManager({});
 const secretName = process.env.SECRET_NAME;
-const app = express();
-
-app.get("*", (req, res) => {
-  res.json({ message: 'What it do, baybee!!!' });
-});
-const server = serverless(app, { basePath: "/" });
 
 exports.handler = async (event, context, callback) => {
   try {
-    if (server) {
-      const result = await server(event, context);
-      return result;
-    }
-    // const { host, port, dbname, username, password } = await getSecretValue(secretName);
-    // const { app } = await createApp({
-    //   storage: {
-    //     storageMethod: 'sql',
-    //     sqlDialect: 'postgres',
-    //     sqlConnectionSsl: true,
-    //     sqlConnectionUrl: `postgresql://${username}:${password}@${host}:${port}/${dbname}`,
-    //     sqlDialectOptions: {
-    //       ssl: true,
-    //     },
-    //   }
-    // });
-    // server = serverless(app);
-    // const result = await server(event, context);
-    // return result;
+    if (!handler) {
+      const { host, port, dbname, username, password } = await getSecretValue(secretName);
+      const { app } = await createApp({
+      storage: {
+        storageMethod: 'sql',
+        sqlDialect: 'postgres',
+        sqlConnectionSsl: true,
+        sqlConnectionUrl: `postgresql://${username}:${password}@${host}:${port}/${dbname}`,
+        sqlDialectOptions: {
+          ssl: true,
+        },
+      }
+    });
+    handler = serverless(app);
+  }
+  const response = await handler(event, context);
+  return response;
   } catch (err) {
     console.error(err);
     callback(err);
